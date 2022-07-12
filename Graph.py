b8af6439a85581d315715a0ddc613ad01b86a773
heyselt <15tjh2@queensu.ca>
2022-07-11 22:11:02 -0700

    Add non-cyclic condition

--- Graph.py
+++ Graph.py
@@ -12,9 +12,8 @@ class Graph:
         edges = 0 # counter to keep track of number of edges in path
         visited = [False] * self.vertices # List to keep track of which vertices have been visited
         visited[0] = True # Add the first vertex to the set of visited vertices
-        self.path.append([0]) # Add the first vertex to the path 
         
-        while edges < self.vertices - 1:
+        while (edges < self.vertices - 1):
             min = MAX
 
             # Variables to keep track of the indicies of the current best edge in the distance matrix 
@@ -24,14 +23,15 @@ class Graph:
             for i in range(self.vertices):
                 if visited[i]:
                     for j in range(self.vertices):
-                        if not visited[j]:
-                            # If not already in the path, check how the edge compares to current best
+                        if ((not visited[j]) and self.graph[i][j]):
+                            # If not already in the path and distance is not zero, check how the edge compares to current best
                             if min > self.graph[i][j]:
                                 min = self.graph[i][j]
                                 x = i
                                 y = j
 
             visited[y] = True
+            print(str(x) + "-" + str(y) + ":" + str(self.graph[x][y]))
             self.path.append([x, y])
             edges += 1
 
